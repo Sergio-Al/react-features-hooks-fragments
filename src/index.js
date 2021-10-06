75875f114ab81e3f8143bfe798dc862aa9955c82
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-10-06 16:04:23 -0400

    The useState Hook
    Now we are using hooks to our project.
    - This manages the use of state in stateless component.
    - Now this type of components is called functional components.
    - To access we need to use 'useState' as a function and declare our variable state as parameter.
    - to Access to our state with hooks we need to deconstruct our array [first in variable value, second is a function to establish a new value 'setCount'].
    - In our events actions like onClick we can use arrow function to change our value.
    - find more info in the docs.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+
+const App = (props) => {
+  const [count, setCount] = useState(props.defaultCount);
+
+  return (
+    <div>
+      <p>The current count is {count}</p>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(props.defaultCount)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+    </div>
+  );
+};
+
+App.defaultProps = {
+  count: 0,
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App defaultCount={12} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
