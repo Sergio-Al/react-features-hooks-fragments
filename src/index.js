79f6f014e1821e9bba70345f22bca032abd00fa3
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-10-06 16:31:38 -0400

    useState vs setState
    With the use of hooks we need to change traditional use of multiple state values.
    - in useState is no need to use an object to track multiple state values.
    - for track multiple state values we need to use useState for each value.
    - the function to update state values 'setState in this case' will update all the state values, this could cause multiple bugs if we want to use multiple state values in a one useState function. It's not recomended.

--- src/index.js
+++ src/index.js
@@ -3,20 +3,58 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
 const App = (props) => {
-  const [count, setCount] = useState(props.defaultCount);
+  // const [count, setCount] = useState(props.defaultCount);
+  // const [text, setText] = useState("");
+  const [state, setState] = useState({ count: props.count, text: "" });
 
   return (
     <div>
-      <p>The current count is {count}</p>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(props.defaultCount)}>Reset</button>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      <p>
+        The current {state.text || "count"} is {state.count}
+      </p>
+      <button
+        onClick={() =>
+          setState(
+            {
+              //...state,
+              count: state.count - 1,
+            } /* not recomended use ...state*/
+          )
+        }
+      >
+        -1
+      </button>
+      <button onClick={() => setState({ count: props.defaultCount })}>
+        Reset
+      </button>
+      <button onClick={() => setState({ count: state.count + 1 })}>+1</button>
+      <input
+        value={state.text}
+        onChange={(e) => setState({ text: e.target.value })}
+      />
     </div>
   );
 };
 
+// const App = (props) => {
+//   const [count, setCount] = useState(props.defaultCount);
+//   const [text, setText] = useState("");
+
+//   return (
+//     <div>
+//       <p>
+//         The current {text || "count"} is {count}
+//       </p>
+//       <button onClick={() => setCount(count - 1)}>-1</button>
+//       <button onClick={() => setCount(props.defaultCount)}>Reset</button>
+//       <button onClick={() => setCount(count + 1)}>+1</button>
+//       <input value={text} onChange={(e) => setText(e.target.value)} />
+//     </div>
+//   );
+// };
+
 App.defaultProps = {
-  count: 0,
+  defaultCount: 0,
 };
 
 ReactDOM.render(
