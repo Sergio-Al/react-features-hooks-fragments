dff6f055cec28d554c07376502dbcf7c8df5113c
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-10-07 17:13:00 -0400

    Complex State with useState
    We can create complex states with useState.
    - We created a state of notes as an array.
    - The array have objects.
    - Objects have data of other can we assign with other states.
    - We can handle simple data state with inputs.
    - Throughout buttons and submit we can handle our complex array state.
    - All worked as expectly.

--- src/index.js
+++ src/index.js
@@ -36,6 +36,48 @@ const App = (props) => {
   );
 };
 
+const NoteApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes([
+      ...notes,
+      {
+        title,
+        body,
+      },
+    ]);
+    setTitle("");
+    setBody("");
+  };
+
+  const removeNote = (title) => {
+    setNotes(notes.filter((note) => note.title !== title));
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map((note) => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <textarea value={body} onChange={(e) => setBody(e.target.value)} />
+        <button>Add Note</button>
+      </form>
+    </div>
+  );
+};
+
 // const App = (props) => {
 //   const [count, setCount] = useState(props.defaultCount);
 //   const [text, setText] = useState("");
@@ -53,13 +95,9 @@ const App = (props) => {
 //   );
 // };
 
-App.defaultProps = {
-  defaultCount: 0,
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <App defaultCount={12} />
+    <NoteApp />
   </React.StrictMode>,
   document.getElementById("root")
 );
