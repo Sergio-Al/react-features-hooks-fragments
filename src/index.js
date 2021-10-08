9f092cf1628dc17d24b1cd5cd8be4862d5c6227f
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-10-07 20:53:34 -0400

    The useReducer hook
    The useReducer Hook is like redux, but this doesn't mean that replaces redux.
    - Through useReducer we can create an state management with state and action like redux.
    - We can assign action types 'POPULATE_DATA' 'ADD_DATA' 'REMOVE_DATA' and work with the state.
    - We can call every reducer through its function 'dispatch in our case'.
    - Remember! In this example the persistent data is managed by useEffect.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
+const notesReducer = (state, action) => {
+  switch (action.type) {
+    case "POPULATE_NOTES":
+      return action.notes;
+    case "ADD_NOTE":
+      return [...state, { title: action.title, body: action.body }];
+    case "REMOVE_NOTE":
+      return state.filter((note) => note.title !== action.title);
+    default:
+      return state;
+  }
+};
+
 const NoteApp = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(notesReducer, []);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
   const addNote = (e) => {
     e.preventDefault();
-    setNotes([
-      ...notes,
-      {
-        title,
-        body,
-      },
-    ]);
+    dispatch({ type: "ADD_NOTE", title, body });
     setTitle("");
     setBody("");
   };
 
   const removeNote = (title) => {
-    setNotes(notes.filter((note) => note.title !== title));
+    dispatch({ type: "REMOVE_NOTE", title });
   };
 
   useEffect(() => {
-    const initialNotes = JSON.parse(localStorage.getItem("notes"));
-    if (initialNotes) {
-      setNotes(initialNotes);
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    if (notes) {
+      dispatch({ type: "POPULATE_NOTES", notes });
     }
   }, []);
 
